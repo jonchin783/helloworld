e4dfcc0443ba7de1b254e9184c09b0d03f4ad8ce
jonchin <jonchin@google.com>
2022-01-27 21:01:24 +0800

    4th push

--- src/main/java/com/example/helloworld/HelloworldApplication.java
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -1,7 +1,11 @@
 package com.example.helloworld;
+package com.example.helloworld;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class HelloworldApplication {
